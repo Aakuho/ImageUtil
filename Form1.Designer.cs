e86fc3328569fd2e514449eddb17ca1b0f362b82
aakuho <martinsebor06@gmail.com>
2023-05-14 15:14:13 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿
+namespace ImageUtil
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panelConvert = new System.Windows.Forms.Panel();
+            this.btnConvertIndividual = new System.Windows.Forms.Button();
+            this.btnConvertBulk = new System.Windows.Forms.Button();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.panelAbout = new System.Windows.Forms.Panel();
+            this.btnAboutCompany = new System.Windows.Forms.Button();
+            this.btnAboutProject = new System.Windows.Forms.Button();
+            this.btnAbout = new System.Windows.Forms.Button();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.btnAboutSchool = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panelConvert.SuspendLayout();
+            this.panelAbout.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.panelConvert);
+            this.panel1.Controls.Add(this.btnConvert);
+            this.panel1.Controls.Add(this.panelAbout);
+            this.panel1.Controls.Add(this.btnAbout);
+            this.panel1.Controls.Add(this.panelLogo);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 567);
+            this.panel1.TabIndex = 0;
+            // 
+            // panelConvert
+            // 
+            this.panelConvert.Controls.Add(this.button1);
+            this.panelConvert.Controls.Add(this.btnConvertIndividual);
+            this.panelConvert.Controls.Add(this.btnConvertBulk);
+            this.panelConvert.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelConvert.Location = new System.Drawing.Point(0, 335);
+            this.panelConvert.Name = "panelConvert";
+            this.panelConvert.Size = new System.Drawing.Size(200, 135);
+            this.panelConvert.TabIndex = 4;
+            // 
+            // btnConvertIndividual
+            // 
+            this.btnConvertIndividual.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnConvertIndividual.Location = new System.Drawing.Point(0, 45);
+            this.btnConvertIndividual.Name = "btnConvertIndividual";
+            this.btnConvertIndividual.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.btnConvertIndividual.Size = new System.Drawing.Size(200, 45);
+            this.btnConvertIndividual.TabIndex = 4;
+            this.btnConvertIndividual.Text = "Individual";
+            this.btnConvertIndividual.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnConvertIndividual.UseVisualStyleBackColor = true;
+            // 
+            // btnConvertBulk
+            // 
+            this.btnConvertBulk.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnConvertBulk.Location = new System.Drawing.Point(0, 0);
+            this.btnConvertBulk.Name = "btnConvertBulk";
+            this.btnConvertBulk.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.btnConvertBulk.Size = new System.Drawing.Size(200, 45);
+            this.btnConvertBulk.TabIndex = 3;
+            this.btnConvertBulk.Text = "Bulk";
+            this.btnConvertBulk.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnConvertBulk.UseVisualStyleBackColor = true;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnConvert.Location = new System.Drawing.Point(0, 285);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnConvert.Size = new System.Drawing.Size(200, 50);
+            this.btnConvert.TabIndex = 3;
+            this.btnConvert.Text = "CONVERT";
+            this.btnConvert.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnConvert.UseVisualStyleBackColor = true;
+            // 
+            // panelAbout
+            // 
+            this.panelAbout.Controls.Add(this.btnAboutSchool);
+            this.panelAbout.Controls.Add(this.btnAboutCompany);
+            this.panelAbout.Controls.Add(this.btnAboutProject);
+            this.panelAbout.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelAbout.Location = new System.Drawing.Point(0, 150);
+            this.panelAbout.Name = "panelAbout";
+            this.panelAbout.Size = new System.Drawing.Size(200, 135);
+            this.panelAbout.TabIndex = 2;
+            // 
+            // btnAboutCompany
+            // 
+            this.btnAboutCompany.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnAboutCompany.Location = new System.Drawing.Point(0, 45);
+            this.btnAboutCompany.Name = "btnAboutCompany";
+            this.btnAboutCompany.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.btnAboutCompany.Size = new System.Drawing.Size(200, 45);
+            this.btnAboutCompany.TabIndex = 1;
+            this.btnAboutCompany.Text = "About Company";
+            this.btnAboutCompany.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAboutCompany.UseVisualStyleBackColor = true;
+            // 
+            // btnAboutProject
+            // 
+            this.btnAboutProject.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnAboutProject.Location = new System.Drawing.Point(0, 0);
+            this.btnAboutProject.Name = "btnAboutProject";
+            this.btnAboutProject.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.btnAboutProject.Size = new System.Drawing.Size(200, 45);
+            this.btnAboutProject.TabIndex = 0;
+            this.btnAboutProject.Text = "About Project";
+            this.btnAboutProject.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAboutProject.UseVisualStyleBackColor = true;
+            // 
+            // btnAbout
+            // 
+            this.btnAbout.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnAbout.Location = new System.Drawing.Point(0, 100);
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnAbout.Size = new System.Drawing.Size(200, 50);
+            this.btnAbout.TabIndex = 1;
+            this.btnAbout.Text = "ABOUT";
+            this.btnAbout.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAbout.UseVisualStyleBackColor = true;
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(200, 100);
+            this.panelLogo.TabIndex = 0;
+            // 
+            // btnAboutSchool
+            // 
+            this.btnAboutSchool.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnAboutSchool.Location = new System.Drawing.Point(0, 90);
+            this.btnAboutSchool.Name = "btnAboutSchool";
+            this.btnAboutSchool.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.btnAboutSchool.Size = new System.Drawing.Size(200, 45);
+            this.btnAboutSchool.TabIndex = 2;
+            this.btnAboutSchool.Text = "About School";
+            this.btnAboutSchool.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAboutSchool.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.Location = new System.Drawing.Point(0, 90);
+            this.button1.Name = "button1";
+            this.button1.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.button1.Size = new System.Drawing.Size(200, 45);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Per Format";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1040, 567);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panelConvert.ResumeLayout(false);
+            this.panelAbout.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Panel panelAbout;
+        private System.Windows.Forms.Button btnAboutCompany;
+        private System.Windows.Forms.Button btnAboutProject;
+        private System.Windows.Forms.Button btnAbout;
+        private System.Windows.Forms.Panel panelLogo;
+        private System.Windows.Forms.Panel panelConvert;
+        private System.Windows.Forms.Button btnConvertIndividual;
+        private System.Windows.Forms.Button btnConvertBulk;
+        private System.Windows.Forms.Button btnAboutSchool;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
